f0707d5549fac422ca8faa50e58221b866df0bf2
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-05-26 17:44:59 +0200

    Convert into PHP Blocks #2

--- index.php
+++ index.php
@@ -16,20 +16,8 @@
         <header>
             
             <?php include("header.php"); ?> <! Inclure le fichier "header.php" >
-    
-           <a class="menu-bar" data-toggle="collapse" href="#menu">
-            <span class="bars"></span>            
-        </a>
-    	<div class="collapse menu" id="menu">
-            <ul class="list-inline">
-                <li><a href="#">Maçonnerie</a></li>
-                <li><a href="#">Charpente</a></li>
-                <li><a href="#">Façade</a></li>
-                <li><a href="#">Maisons</a></li>
-                <li><a href="#">Réalisation</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>   
-    	</div>
+            
+            <?php include("navbar.php"); ?> <! Inclure le fichier "header.php" >
 
         </header>
     
---
+++ navbar.php
@@ -0,0 +1,16 @@
+<! ------ "Header" : Haut de page (Logo, Contact, Boutons) ------ >
+
+<a class="menu-bar" data-toggle="collapse" href="#menu">
+    <span class="bars"></span>
+</a>
+
+<div class="collapse menu" id="menu">
+    <ul class="list-inline">
+        <li><a href="#">Maçonnerie</a></li>
+        <li><a href="#">Charpente</a></li>
+        <li><a href="#">Façade</a></li>
+        <li><a href="#">Maisons</a></li>
+        <li><a href="#">Réalisation</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</div>
