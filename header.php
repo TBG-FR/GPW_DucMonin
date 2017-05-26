3b7da998440fa38dbe59ddf67500985cd1c5cb1d
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-05-26 17:39:17 +0200

    Convert to PHP Blocks - Beginning

---
+++ header.php
@@ -0,0 +1,23 @@
+<! ------ "Header" : Haut de page (Logo, Contact, Boutons) ------ >
+
+<div id="header_head">
+    <div class="lamd3">
+        <img src="logo.png" alt="Logo de Ducrozet-Moninot" id="logo">
+    </div>
+    
+    <div class="lamd5">
+        <div id="header_contact">
+            99 Chemin de l'Étang Neuf
+            <br/>01000 St-Denis-lès-Bourg
+            <br/>Tel: 04.74.11.22.33
+        </div>
+    </div>
+    
+    <div class="lamd4">
+        <div id="header_buttons">
+            <div class="glyphicon glyphicon-envelope" aria-label="Bouton-Icône : Contact"></div>
+            <div class="glyphicon glyphicon-earphone" aria-label="Bouton-Icône : Téléphone"></div>
+            <div class="glyphicon glyphicon-map-marker" aria-label="Bouton-Icône : Localisation"></div>
+        </div>
+    </div>
+</div>
