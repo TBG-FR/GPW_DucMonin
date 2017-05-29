468d51afc78f8cc0c774f9b45abfdacc403d315c
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-05-29 22:50:57 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,19 +1,11 @@
-<!-- ------ "Navbar" : Menu de Navigation ------ -->
-
-<!-- <a class="menu-bar" data-toggle="collapse" href="#menu">
-    <span class="bars"></span>
-</a>*/ -->
-
-<div class="collapse menu" id="menu">
-    <ul class="list-inline">
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="maconnerie.php">Maçonnerie</a></li>
-        <li><a href="charpente.php">Charpente</a></li>
-        <li><a href="facade.php">Façade</a></li>
-        <li><a href="maisons.php">Maisons</a></li>
-        <li><a href="realisations.php">Réalisations</a></li>
-        <li><a href="contact.php">Contact</a></li>
-    </ul>
-</div>
-
-<!-- ------ "Navbar" : Fin du Bloc ------ -->
+ <div class="collapse menu" id="menu">
+      <ul class="list-inline">
+ +        <li><a href="index.php">Accueil</a></li>
+ +        <li><a href="maconnerie.php">Maçonnerie</a></li>
+ +        <li><a href="charpente.php">Charpente</a></li>
+ +        <li><a href="facade.php">Façade</a></li>
+ +        <li><a href="maisons.php">Maisons</a></li>
+ +        <li><a href="realisations.php">Réalisations</a></li>
+ +        <li><a href="contact.php">Contact</a></li>
+      </ul>
+  </div>
