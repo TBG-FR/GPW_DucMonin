53a38246ef73fa6868f2b518bbcb09237affc3d7
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-05-29 22:51:26 +0200

    Update maconnerie.php

--- maconnerie.php
+++ maconnerie.php
@@ -1,63 +1,63 @@
-<!DOCTYPE html>
-<html>    
-    <head>
-        <meta charset="utf-8">
-        
-        <title>Maçonnerie - Ducrozet-Moninot</title>
-                
-        <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <!-- Custom CSS -->
-        <link rel="stylesheet" href="style.css">
-        
-    </head>
-    
-    
-    <body>
-        <header>
-            
-            <?php include("header.php"); ?>
-            
-            <?php include("navbar.php"); ?>
-
-        </header>
-    
-        <div id="main">
-        
-            <section id="content">
-                <h1>Maçonnerie</h1>
-                
-                <div class="description">
-                    <p>                        
-                        Ducrozet-Moninot met son expérience et ses compétences à votre service pour concrétiser vos projets de <strong>construction</strong> ou de <strong>rénovation</strong>.<br />
-                        Un large éventail de prestations est réalisé par nos artisans dans divers domaines.                        
-                    </p>
-                    <p>
-                        <h3>Travaux de Maçonnerie générale :</h3>
-                        <ul>
-                            <li>Construction de maisons individuelles (Nous proposons une <a href="maisons.php">réalisation complète du gros oeuvre de votre villa</a>).</li>
-                            <li>Extensions de maison (Extension traditionnelle, Toiture, Terrasse, ...)</li>
-                            <li>Rénovation de bâtiments anciens (Démolition et Rénovation).</li>
-                            <li>Ouverture en sous œuvre (Mur porteur ou non, mise en place IPN ou HEA).</li>
-                            <li>Construction de piscines en béton armé.</li>
-                            <li>Mur de clôture, terrasse, auvent, garage, ...</li>
-                        </ul>
-                    
-                    N'hésitez pas à nous contacter pour de plus amples informations, ou même à nous demander un devis détaillé et gratuit !
-                
-                </p>
-                
-                </div>
-                
-            </section>
-            
-            <?php include("sidebar.php"); ?> <! Inclure le fichier "sidebar.php" >
-       
-        </div>
-    
-    <footer>
-        <?php include("footer.php"); ?>
-    </footer>
-        
-    </body>    
-</html>
++<!DOCTYPE html>
+ +<html>    
+ +    <head>
+ +        <meta charset="utf-8">
+ +        
+ +        <title>Maçonnerie - Ducrozet-Moninot</title>
+ +                
+ +        <!-- Bootstrap -->
+ +        <link href="css/bootstrap.min.css" rel="stylesheet">
+ +        <!-- Custom CSS -->
+ +        <link rel="stylesheet" href="style.css">
+ +        
+ +    </head>
+ +    
+ +    
+ +    <body>
+ +        <header>
+ +            
+ +            <?php include("header.php"); ?>
+ +            
+ +            <?php include("navbar.php"); ?>
+ +
+ +        </header>
+ +    
+ +        <div id="main">
+ +        
+ +            <section id="content">
+ +                <h1>Maçonnerie</h1>
+ +                
+ +                <div class="description">
+ +                    <p>                        
+ +                        Ducrozet-Moninot met son expérience et ses compétences à votre service pour concrétiser vos projets de <strong>construction</strong> ou de <strong>rénovation</strong>.<br />
+ +                        Un large éventail de prestations est réalisé par nos artisans dans divers domaines.                        
+ +                    </p>
+ +                    <p>
+ +                        <h3>Travaux de Maçonnerie générale :</h3>
+ +                        <ul>
+ +                            <li>Construction de maisons individuelles (Nous proposons une <a href="maisons.php">réalisation complète du gros oeuvre de votre villa</a>).</li>
+ +                            <li>Extensions de maison (Extension traditionnelle, Toiture, Terrasse, ...)</li>
+ +                            <li>Rénovation de bâtiments anciens (Démolition et Rénovation).</li>
+ +                            <li>Ouverture en sous œuvre (Mur porteur ou non, mise en place IPN ou HEA).</li>
+ +                            <li>Construction de piscines en béton armé.</li>
+ +                            <li>Mur de clôture, terrasse, auvent, garage, ...</li>
+ +                        </ul>
+ +                    
+ +                    N'hésitez pas à nous contacter pour de plus amples informations, ou même à nous demander un devis détaillé et gratuit !
+ +                
+ +                </p>
+ +                
+ +                </div>
+ +                
+ +            </section>
+ +            
+ +            <?php include("sidebar.php"); ?> <! Inclure le fichier "sidebar.php" >
+ +       
+ +        </div>
+ +    
+ +    <footer>
+ +        <?php include("footer.php"); ?>
+ +    </footer>
+ +        
+ +    </body>    
+ +</html> 
