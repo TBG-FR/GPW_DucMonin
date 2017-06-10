1a46cf42aceb8afc13c390721ff3a12273960245
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-06-10 22:16:02 +0200

    Footer++

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <!-- ------ "Footer" : Bas de Page ------ -->
 
 <p id="legal">
-    SARL Ducrozet-Moninot (c) | 200X-2017 | Tous droits réservés
+    SARL Ducrozet-Moninot (c) | 2000-2017 | Tous droits réservés.
     <br/>Réalisation : Dyvia F. / Simon H. / Florent P. / Tom-Brian G.
 </p>
 
