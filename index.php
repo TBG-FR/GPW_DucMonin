235d8e92e5114755fc37b67d85e15f4b16cee5ec
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-06-11 23:00:57 +0200

    Index Update - Fixs

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
         <!-- Custom CSS -->
         <link rel="stylesheet" href="globalstyle.css">
         <link rel="stylesheet" href="style.css">
+        <!-- Javascript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 
     </head>
 
@@ -21,20 +24,22 @@
             <?php include("navbar.php"); ?>
 
         </header>
-        
+
         <div id="main">
-            <div class=container-fluid>
-                <div class="TEMP">
-                    
-                    <map name="map1">
-                        <area href="#" shape="rect" coords="0,152,140,80" title="Maçonnerie" tabindex="0" />
-                        <area href="#" shape="rect" coords="0,100,50,100" title="Charpente" tabindex="0"/>
-                        <area href="#" shape="rect" coords="2,10,20,110" title="Façade" tabindex="0"/>
-                        <area href="#" shape="rect" coords="0,30,100,130" title="Maison" tabindex="0"/>
-                    </map>
-                    
-                    <img src="maison02.PNG" usemap="#map1" alt="TODO" title="TODO">
-                
+            <div id="accueil">
+                <div class=container-fluid>
+                    <div class="TEMP">
+                        
+                        <map name="map1">
+                            <area href="#" shape="rect" coords="0,152,140,80" title="Maçonnerie" tabindex="0" />
+                            <area href="#" shape="rect" coords="0,100,50,100" title="Charpente" tabindex="0"/>
+                            <area href="#" shape="rect" coords="2,10,20,110" title="Façade" tabindex="0"/>
+                            <area href="#" shape="rect" coords="0,30,100,130" title="Maison" tabindex="0"/>
+                        </map>
+                        
+                        <img src="maison02.PNG" usemap="#map1" alt="TODO" title="TODO">
+                        
+                    </div>
                 </div>
             </div>
         </div>
