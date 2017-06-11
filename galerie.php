09f7a7935c8d21cf4744a36ba7ae354865e882db
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-06-11 23:23:20 +0200

    Galerie - The Hallelujah Update !

--- galerie.php
+++ galerie.php
@@ -1,247 +1,248 @@
+
+
+
 <!DOCTYPE html>
-<html>    
+<html>
     <head>
         <meta charset="utf-8">
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">     </script>
-        
-        <SCRIPT language="javascript">
-        $(document).ready(function(){
 
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
-        }
-    });
-    
-    if ($(".filter-button").removeClass("active")) {
-$(this).removeClass("active");
-}
-$(this).addClass("active");
+        <title>Travaux de Charpente et Couverture - Ducrozet-Moninot</title>
 
-});
-        
-        
-        </SCRIPT>
-        
-        
-        <title>TODO</title>
-                
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <!-- Custom CSS -->
+        <link rel="stylesheet" href="globalstyle.css">
         <link rel="stylesheet" href="style.css">
-        
+        <!-- Javascript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Scripts Javascript -->
+        <SCRIPT language="javascript">
+            $(document).ready(function(){
+                $(".filter-button").click(function(){
+                    var value = $(this).attr('data-filter');
+                    if(value == "all")
+                    {
+                        //$('.filter').removeClass('hidden');
+                        $('.filter').show('1000');
+                    }
+                    else
+                    {
+                        //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+                        //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+                        $(".filter").not('.'+value).hide('3000');
+                        $('.filter').filter('.'+value).show('3000');
+
+                    }
+                });
+
+                if ($(".filter-button").removeClass("active")) {
+                    $(this).removeClass("active");
+                }
+                $(this).addClass("active");
+
+            });
+        </SCRIPT>
+
     </head>
-    
-    
+
+
     <body>
         <header>
-            
+
             <?php include("header.php"); ?>
-            
+
             <?php include("navbar.php"); ?>
 
         </header>
-    
+
         <div id="main">
-        
+
             <section id="content">
-                
-            <div id="gallery_buttons">
-                <button class="btn btn-default filter-button" data-filter="all">Tout</button>
-                <button class="btn btn-default filter-button" data-filter="charpente">Charpente et Couverture</button>
-                <button class="btn btn-default filter-button" data-filter="facade">Facade</button>
-                <button class="btn btn-default filter-button" data-filter="maconnerie">Maçonnerie</button>
-                <button class="btn btn-default filter-button" data-filter="maison">Maisons Individuelles</button>
-            </div>                
-                
+
+                <div id="gallery_buttons">
+                    <button class="btn btn-default filter-button" data-filter="all">Tout</button>
+                    <button class="btn btn-default filter-button" data-filter="charpente">Charpente et Couverture</button>
+                    <button class="btn btn-default filter-button" data-filter="facade">Facade</button>
+                    <button class="btn btn-default filter-button" data-filter="maconnerie">Maçonnerie</button>
+                    <button class="btn btn-default filter-button" data-filter="maison">Maisons Individuelles</button>
+                </div>                
+
                 <div class="gallery filter charpente">
                     <a target="_blank" href="charpente01.jpg">
-                        <img src="charpente01.jpg" alt="réalisation 2012">
+                        <img src="images/charpente01.jpg" alt="réalisation 2012">
                     </a>
                 </div>
 
                 <div class="gallery filter charpente">
-                    <a target="_blank" href="charpente02.jpg">
-                        <img src="charpente02.jpg" alt="Bourg-enBresse 2008">
+                    <a target="_blank" href="images/charpente02.jpg">
+                        <img src="images/charpente02.jpg" alt="Bourg-enBresse 2008">
                     </a>
                 </div>
 
                 <div class="gallery filter charpente">
-                    <a target="_blank" href="charpente03.jpg">
-                        <img src="charpente03.jpg" alt="Bip bap boup">
+                    <a target="_blank" href="images/charpente03.jpg">
+                        <img src="images/charpente03.jpg" alt="Bip bap boup">
                     </a>
                 </div>
 
                 <div class="gallery filter charpente">
-                    <a target="_blank" href="charpente04.jpg">
-                        <img src="charpente04.jpg" alt="Ex photo de background">
+                    <a target="_blank" href="images/charpente04.jpg">
+                        <img src="images/charpente04.jpg" alt="Ex photo de background">
                     </a>
                 </div>
-                
+
                 <div class="gallery filter facade">
-                    <a target="_blank" href="facade01.jpg">
-                        <img src="facade01.jpg">
+                    <a target="_blank" href="images/facade01.jpg">
+                        <img src="images/facade01.jpg">
                     </a>
                 </div>
-                
-                
+
+
                 <div class="gallery filter facade">
-                    <a target="_blank" href="facade02.JPG">
-                        <img src="facade02.JPG">
+                    <a target="_blank" href="images/facade02.JPG">
+                        <img src="images/facade02.JPG">
                     </a>
                 </div>
-                
+
                 <div class="gallery filter facade">
-                    <a target="_blank" href="facade03.jpg">
-                        <img src="facade03.jpg">
+                    <a target="_blank" href="images/facade03.jpg">
+                        <img src="images/facade03.jpg">
                     </a>
                 </div>
-                
+
                 <div class="gallery filter facade">
-                    <a target="_blank" href="facade04.JPG">
-                        <img src="facade04.JPG">
+                    <a target="_blank" href="images/facade04.JPG">
+                        <img src="images/facade04.JPG">
                     </a>
                 </div>
-                
+
                 <div class="gallery filter facade">
-                    <a target="_blank" href="facade05.jpg">
-                        <img src="facade05.jpg">
+                    <a target="_blank" href="images/facade05.jpg">
+                        <img src="images/facade05.jpg">
                     </a>
                 </div>
-                
+
                 <div class="gallery filter facade">
-                    <a target="_blank" href="facade06.JPG">
-                        <img src="facade06.JPG">
+                    <a target="_blank" href="images/facade06.JPG">
+                        <img src="images/facade06.JPG">
                     </a>
                 </div>
-                
+
                 <div class="gallery  filter facade">
-                    <a target="_blank" href="facade07.JPG">
-                        <img src="facade07.JPG">
-                    </a>
-                </div>
-                
-                <div class="gallery filter facade">
-                    <a target="_blank" href="facade08.JPG">
-                        <img src="facade08.JPG">
-                    </a>
-                </div>
-                
-                <div class="gallery filter facade">
-                    <a target="_blank" href="facade09.JPG">
-                        <img src="facade09.JPG">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maconnerie">
-                    <a target="_blank" href="maconnerie01.jpg">
-                        <img src="maconnerie01.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maconnerie">
-                    <a target="_blank" href="maconnerie02.jpg">
-                        <img src="maconnerie02.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maconnerie">
-                    <a target="_blank" href="maconnerie03.jpg">
-                        <img src="maconnerie03.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maconnerie">
-                    <a target="_blank" href="maconnerie04.jpg">
-                        <img src="maconnerie04.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maconnerie">
-                    <a target="_blank" href="maconnerie05.jpg">
-                        <img src="maconnerie05.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maconnerie">
-                    <a target="_blank" href="maconnerie06.jpg">
-                        <img src="maconnerie06.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maconnerie">
-                    <a target="_blank" href="maconnerie07.jpg">
-                        <img src="maconnerie07.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maison">
-                    <a target="_blank" href="maison01.jpg">
-                        <img src="maison01.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maison">
-                    <a target="_blank" href="maison02.jpg">
-                        <img src="maison02.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maison">
-                    <a target="_blank" href="maison03.jpg">
-                        <img src="maison03.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maison">
-                    <a target="_blank" href="maison04.jpg">
-                        <img src="maison04.jpg">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maison">
-                    <a target="_blank" href="maison05.png">
-                        <img src="maison05.png">
-                    </a>
-                </div>
-                
-                <div class="gallery filter maison">
-                    <a target="_blank" href="maison06.JPG">
-                        <img src="maison06.JPG">
-                    </a>
-                </div>
-                
-                 <div class="gallery filter maison">
-                    <a target="_blank" href="maison07.JPG">
-                        <img src="maison07.JPG">
+                    <a target="_blank" href="images/facade07.JPG">
+                        <img src="images/facade07.JPG">
                     </a>
                 </div>
 
-                
-                
+                <div class="gallery filter facade">
+                    <a target="_blank" href="images/facade08.JPG">
+                        <img src="images/facade08.JPG">
+                    </a>
+                </div>
+
+                <div class="gallery filter facade">
+                    <a target="_blank" href="images/facade09.JPG">
+                        <img src="images/facade09.JPG">
+                    </a>
+                </div>
+
+                <div class="gallery filter maconnerie">
+                    <a target="_blank" href="images/maconnerie01.jpg">
+                        <img src="images/maconnerie01.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maconnerie">
+                    <a target="_blank" href="images/maconnerie02.jpg">
+                        <img src="images/maconnerie02.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maconnerie">
+                    <a target="_blank" href="images/maconnerie03.jpg">
+                        <img src="images/maconnerie03.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maconnerie">
+                    <a target="_blank" href="images/maconnerie04.jpg">
+                        <img src="images/maconnerie04.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maconnerie">
+                    <a target="_blank" href="images/maconnerie05.jpg">
+                        <img src="images/maconnerie05.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maconnerie">
+                    <a target="_blank" href="images/maconnerie06.jpg">
+                        <img src="images/maconnerie06.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maconnerie">
+                    <a target="_blank" href="images/maconnerie07.jpg">
+                        <img src="images/maconnerie07.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maison">
+                    <a target="_blank" href="images/maison01.jpg">
+                        <img src="images/maison01.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maison">
+                    <a target="_blank" href="images/maison02.jpg">
+                        <img src="images/maison02.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maison">
+                    <a target="_blank" href="images/maison03.jpg">
+                        <img src="images/maison03.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maison">
+                    <a target="_blank" href="images/maison04.jpg">
+                        <img src="images/maison04.jpg">
+                    </a>
+                </div>
+
+                <div class="gallery filter maison">
+                    <a target="_blank" href="images/maison05.png">
+                        <img src="images/maison05.png">
+                    </a>
+                </div>
+
+                <div class="gallery filter maison">
+                    <a target="_blank" href="images/maison06.JPG">
+                        <img src="images/maison06.JPG">
+                    </a>
+                </div>
+
+                <div class="gallery filter maison">
+                    <a target="_blank" href="images/maison07.JPG">
+                        <img src="images/maison07.JPG">
+                    </a>
+                </div>
+
+
+
             </section>
-        
-     <?php include("sidebar.php"); ?> <! Inclure le fichier "sidebar.php" >
-       
+
+            <?php include("sidebar.php"); ?> <! Inclure le fichier "sidebar.php" >
+
         </div>
-    
-    <footer>
-        <?php include("footer.php"); ?>
-    </footer>
-        
+
+        <footer>
+            <?php include("footer.php"); ?>
+        </footer>
+
     </body>    
 </html>
