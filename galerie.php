3bdaa038271a2abf9ebc5ff2816ddb4e66c68419
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-06-12 10:28:24 +0200

    Update of the Shlag :+1:

--- galerie.php
+++ galerie.php
@@ -67,165 +67,165 @@
                 </div>                
 
                 <div class="gallery filter charpente">
-                    <a target="_blank" href="charpente01.jpg">
-                        <img src="images/charpente01.jpg" alt="réalisation 2012">
+                    <a target="_blank" href="images/vrac/charpente01.jpg">
+                        <img src="images/vrac/charpente01.jpg" alt="réalisation 2012">
                     </a>
                 </div>
 
                 <div class="gallery filter charpente">
-                    <a target="_blank" href="images/charpente02.jpg">
-                        <img src="images/charpente02.jpg" alt="Bourg-enBresse 2008">
+                    <a target="_blank" href="images/vrac/charpente02.jpg">
+                        <img src="images/vrac/charpente02.jpg" alt="Bourg-enBresse 2008">
                     </a>
                 </div>
 
                 <div class="gallery filter charpente">
-                    <a target="_blank" href="images/charpente03.jpg">
-                        <img src="images/charpente03.jpg" alt="Bip bap boup">
+                    <a target="_blank" href="images/vrac/charpente03.jpg">
+                        <img src="images/vrac/charpente03.jpg" alt="Bip bap boup">
                     </a>
                 </div>
 
                 <div class="gallery filter charpente">
-                    <a target="_blank" href="images/charpente04.jpg">
-                        <img src="images/charpente04.jpg" alt="Ex photo de background">
+                    <a target="_blank" href="images/vrac/charpente04.jpg">
+                        <img src="images/vrac/charpente04.jpg" alt="Ex photo de background">
                     </a>
                 </div>
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade01.jpg">
-                        <img src="images/facade01.jpg">
+                    <a target="_blank" href="images/vrac/facade01.jpg">
+                        <img src="images/vrac/facade01.jpg">
                     </a>
                 </div>
 
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade02.JPG">
-                        <img src="images/facade02.JPG">
+                    <a target="_blank" href="images/vrac/facade02.JPG">
+                        <img src="images/vrac/facade02.JPG">
                     </a>
                 </div>
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade03.jpg">
-                        <img src="images/facade03.jpg">
+                    <a target="_blank" href="images/vrac/facade03.jpg">
+                        <img src="images/vrac/facade03.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade04.JPG">
-                        <img src="images/facade04.JPG">
+                    <a target="_blank" href="images/vrac/facade04.JPG">
+                        <img src="images/vrac/facade04.JPG">
                     </a>
                 </div>
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade05.jpg">
-                        <img src="images/facade05.jpg">
+                    <a target="_blank" href="images/vrac/facade05.jpg">
+                        <img src="images/vrac/facade05.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade06.JPG">
-                        <img src="images/facade06.JPG">
+                    <a target="_blank" href="images/vrac/facade06.JPG">
+                        <img src="images/vrac/facade06.JPG">
                     </a>
                 </div>
 
                 <div class="gallery  filter facade">
-                    <a target="_blank" href="images/facade07.JPG">
-                        <img src="images/facade07.JPG">
+                    <a target="_blank" href="images/vrac/facade07.JPG">
+                        <img src="images/vrac/facade07.JPG">
                     </a>
                 </div>
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade08.JPG">
-                        <img src="images/facade08.JPG">
+                    <a target="_blank" href="images/vrac/facade08.JPG">
+                        <img src="images/vrac/facade08.JPG">
                     </a>
                 </div>
 
                 <div class="gallery filter facade">
-                    <a target="_blank" href="images/facade09.JPG">
-                        <img src="images/facade09.JPG">
+                    <a target="_blank" href="images/vrac/facade09.JPG">
+                        <img src="images/vrac/facade09.JPG">
                     </a>
                 </div>
 
                 <div class="gallery filter maconnerie">
-                    <a target="_blank" href="images/maconnerie01.jpg">
-                        <img src="images/maconnerie01.jpg">
+                    <a target="_blank" href="images/vrac/maconnerie01.jpg">
+                        <img src="images/vrac/maconnerie01.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maconnerie">
-                    <a target="_blank" href="images/maconnerie02.jpg">
-                        <img src="images/maconnerie02.jpg">
+                    <a target="_blank" href="images/vrac/maconnerie02.jpg">
+                        <img src="images/vrac/maconnerie02.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maconnerie">
-                    <a target="_blank" href="images/maconnerie03.jpg">
-                        <img src="images/maconnerie03.jpg">
+                    <a target="_blank" href="images/vrac/maconnerie03.jpg">
+                        <img src="images/vrac/maconnerie03.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maconnerie">
-                    <a target="_blank" href="images/maconnerie04.jpg">
-                        <img src="images/maconnerie04.jpg">
+                    <a target="_blank" href="images/vrac/maconnerie04.jpg">
+                        <img src="images/vrac/maconnerie04.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maconnerie">
-                    <a target="_blank" href="images/maconnerie05.jpg">
-                        <img src="images/maconnerie05.jpg">
+                    <a target="_blank" href="images/vrac/maconnerie05.jpg">
+                        <img src="images/vrac/maconnerie05.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maconnerie">
-                    <a target="_blank" href="images/maconnerie06.jpg">
-                        <img src="images/maconnerie06.jpg">
+                    <a target="_blank" href="images/vrac/maconnerie06.jpg">
+                        <img src="images/vrac/maconnerie06.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maconnerie">
-                    <a target="_blank" href="images/maconnerie07.jpg">
-                        <img src="images/maconnerie07.jpg">
+                    <a target="_blank" href="images/vrac/maconnerie07.jpg">
+                        <img src="images/vrac/maconnerie07.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maison">
-                    <a target="_blank" href="images/maison01.jpg">
-                        <img src="images/maison01.jpg">
+                    <a target="_blank" href="images/vrac/maison01.jpg">
+                        <img src="images/vrac/maison01.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maison">
-                    <a target="_blank" href="images/maison02.jpg">
-                        <img src="images/maison02.jpg">
+                    <a target="_blank" href="images/vrac/maison02.jpg">
+                        <img src="images/vrac/maison02.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maison">
-                    <a target="_blank" href="images/maison03.jpg">
-                        <img src="images/maison03.jpg">
+                    <a target="_blank" href="images/vrac/maison03.jpg">
+                        <img src="images/vrac/maison03.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maison">
-                    <a target="_blank" href="images/maison04.jpg">
-                        <img src="images/maison04.jpg">
+                    <a target="_blank" href="images/vrac/maison04.jpg">
+                        <img src="images/vrac/maison04.jpg">
                     </a>
                 </div>
 
                 <div class="gallery filter maison">
-                    <a target="_blank" href="images/maison05.png">
-                        <img src="images/maison05.png">
+                    <a target="_blank" href="images/vrac/maison05.png">
+                        <img src="images/vrac/maison05.png">
                     </a>
                 </div>
 
                 <div class="gallery filter maison">
-                    <a target="_blank" href="images/maison06.JPG">
-                        <img src="images/maison06.JPG">
+                    <a target="_blank" href="images/vrac/maison06.JPG">
+                        <img src="images/vrac/maison06.JPG">
                     </a>
                 </div>
 
                 <div class="gallery filter maison">
-                    <a target="_blank" href="images/maison07.JPG">
-                        <img src="images/maison07.JPG">
+                    <a target="_blank" href="images/vrac/maison07.JPG">
+                        <img src="images/vrac/maison07.JPG">
                     </a>
                 </div>
 
